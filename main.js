1e9079c6aff9818f5877eb74168e2cd4d3bc57db
inglyd <inglydalves@outlook.com>
2021-07-14 16:02:58 -0300

    add congifurações do CarGame

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-import './style.css'
+import "./src/styles/settings/colors.css"
+impor
+import CardGame from "./src/components/CardGame";
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const $root = document.querySelector("#root");
+const $htmlCardGame = CardGame();
+
+$root.insertAdjacentHTML("beforeend", $htmlCardGame);
+
+
+
